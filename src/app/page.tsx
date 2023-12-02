9357fa559d780045300e0031b6fb4309b8e1546f
taro-28 <tarotaro0208@gmail.com>
2023-12-02 20:43:15 +0900

    Remove unused code and console.log statement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
-import { request } from "@/functions/getToken";
 import { UserButton } from "@clerk/nextjs";
 
 export default async function Home() {
-  const hoge = await request();
   return (
     <main>
       <UserButton afterSignOutUrl="/" />
-      {JSON.stringify(hoge)}
     </main>
   );
 }
