145c0c93da4ecc079240ec19199940041f56cb13
taro-28 <tarotaro0208@gmail.com>
2023-12-09 08:48:53 +0900

    Add create functionality for todos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import { Card } from "@/components/Card";
 import { PageTitle } from "@/components/PageTitle";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { TODO } from "@/features/todo";
 import { gqlRequest } from "@/functions/gqlRequest";
 import { ComponentPropsWithoutRef } from "react";
+import { z } from "zod";
 import { columns } from "./columns";
 import { DataTable } from "./data-table";
 
@@ -45,13 +48,56 @@ const todos: ComponentPropsWithoutRef<typeof Card>[] = [
   },
 ];
 
+const schema = z.object({
+  content: z.string().min(1),
+});
+
 export default async function Home() {
   const { data } = (await gqlRequest({
-    query: "query {todos {id text}}",
+    query: "query {todos {id content}}",
   })) as { data: { todos: TODO[] } };
+
+  async function create(formData: FormData) {
+    "use server";
+
+    const validatedFields = schema.safeParse({
+      content: formData.get("content"),
+    });
+
+    // Return early if the form data is invalid
+    if (!validatedFields.success) {
+      return {
+        errors: validatedFields.error.flatten().fieldErrors,
+      };
+    }
+
+    console.log(
+      validatedFields.data.content,
+      `mutation createTodo {
+      createTodo(input: {content: ${validatedFields.data.content}}) {
+        id
+      }
+    }`
+    );
+    await gqlRequest({
+      query: `mutation createTodo {
+        createTodo(input: {content: "${validatedFields.data.content}"}) {
+          id
+        }
+      }`,
+    });
+
+    return "";
+  }
+
   return (
     <div className="w-full space-y-4">
       <PageTitle>Todo</PageTitle>
+      <form action={create} className="flex space-x-2">
+        <Input name="content" />
+        <Button>Create</Button>
+      </form>
+
       <DataTable columns={columns} data={data.todos} />
     </div>
   );
