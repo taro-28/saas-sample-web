a45276cb1bde4770f25f616936b834c128f58984
taro-28 <tarotaro0208@gmail.com>
2023-12-27 23:16:05 +0900

    Add SubmitButton component and update CreateForm

---
+++ src/components/SubmitButton.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { ComponentPropsWithoutRef } from "react";
+import { useFormStatus } from "react-dom";
+
+type Props = Omit<ComponentPropsWithoutRef<typeof Button>, "type">;
+
+export const SubmitButton = ({ disabled, ...props }: Props) => {
+  const { pending } = useFormStatus();
+  return <Button {...props} type="submit" disabled={disabled || pending} />;
+};
