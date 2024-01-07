4c2e1bc45f0e6c8c739f11db13247112f9263740
taro-28 <tarotaro0208@gmail.com>
2024-01-07 12:41:25 +0900

    Refactor ResizablePanelGroup and ResizableHandle imports

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -4,6 +4,7 @@ import { GripVertical } from "lucide-react";
 import * as ResizablePrimitive from "react-resizable-panels";
 
 import { cn } from "@/lib/utils";
+import { ComponentProps } from "react";
 
 const ResizablePanelGroup = ({
   className,
@@ -11,12 +12,12 @@ const ResizablePanelGroup = ({
     document.cookie = `react-resizable-panels:layout=${JSON.stringify(sizes)}`;
   },
   ...props
-}: React.ComponentProps<typeof ResizablePrimitive.PanelGroup>) => {
+}: ComponentProps<typeof ResizablePrimitive.PanelGroup>) => {
   return (
     <ResizablePrimitive.PanelGroup
       className={cn(
         "flex h-full w-full data-[panel-group-direction=vertical]:flex-col",
-        className
+        className,
       )}
       onLayout={onLayout}
       {...props}
@@ -30,13 +31,13 @@ const ResizableHandle = ({
   withHandle,
   className,
   ...props
-}: React.ComponentProps<typeof ResizablePrimitive.PanelResizeHandle> & {
+}: ComponentProps<typeof ResizablePrimitive.PanelResizeHandle> & {
   withHandle?: boolean;
 }) => (
   <ResizablePrimitive.PanelResizeHandle
     className={cn(
       "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90",
-      className
+      className,
     )}
     {...props}
   >
