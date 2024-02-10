507873a17c4be20aa754f29f229c6d8a71172c41
taro-28 <tarotaro0208@gmail.com>
2024-02-10 22:30:41 +0900

    Update config in knip.ts

--- knip.ts
+++ knip.ts
@@ -1,14 +1,18 @@
 const config = {
   project: ["src/**/*.ts", "src/**/*.tsx", "src/**/*.js", "src/**/*.jsx"],
-  ignore: ["src/gql/generated.ts", "src/components/ui/**/*.{ts,tsx}"],
+  ignore: [
+    "src/gql/generated.ts",
+    "src/components/ui/**/*.{ts,tsx}",
+    ".github/workflows/ci.yml",
+    ".github/workflows/nextjs_bundle_analysis.yml",
+  ],
   ignoreDependencies: [
     "@biomejs/biome",
-    "gzip-size",
-    "mkdirp",
     "@graphql-codegen/typescript",
     "@graphql-codegen/typescript-graphql-request",
     "@graphql-codegen/typescript-operations",
   ],
+  ignoreBinaries: ["check", "type-check"],
 };
 
 export default config;
