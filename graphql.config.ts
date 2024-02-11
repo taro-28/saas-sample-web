d8a237ab2988af998f67fcc2e1b73652b13f67d7
taro-28 <tarotaro0208@gmail.com>
2024-02-11 10:46:34 +0900

    Update graphql.config.ts to use client preset and add fragment masking

--- graphql.config.ts
+++ graphql.config.ts
@@ -3,19 +3,12 @@ import type { IGraphQLConfig } from "graphql-config";
 
 const codegenConfig: CodegenConfig = {
   generates: {
-    "./src/gql/generated.ts": {
-      plugins: [
-        "typescript",
-        "typescript-operations",
-        "typescript-graphql-request",
-      ],
+    "./src/gql/": {
+      preset: "client",
+      presetConfig: {
+        fragmentMasking: { unmaskFunctionName: "getFragmentData" },
+      },
     },
-  },
-  hooks: {
-    afterOneFileWrite: [
-      // https://github.com/dotansimha/graphql-code-generator-community/issues/501#issuecomment-1817878263
-      "sed -i '' -e '1s|.*|import type { GraphQLClient, RequestOptions } from \"graphql-request\";|' -e '2s|.*|type GraphQLClientRequestHeaders = RequestOptions[\"requestHeaders\"];|' src/gql/generated.ts",
-    ],
   },
 };
 
